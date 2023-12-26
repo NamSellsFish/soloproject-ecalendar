19acd86c253352fa72ff7f747c3fbc4a379e7712
NamSellsFish <nhatnam0192123@gmail.com>
2023-12-26 14:38:58 +0700

    create eCalendar page

---
+++ tier-2/e-calendar/src/app/e-calendar/page.tsx
@@ -0,0 +1,14 @@
+const ecalendarPage = () => {
+    return (
+    <main className="flex w-3/4 h-fit m-[152px] bg-[url('https://i.pinimg.com/originals/02/82/96/028296f054dee4f2bbc9c68994cbfe70.png')]">
+        <section className="w-8/12 min-h-screen bg-red-500 bg-opacity-50">
+                <h1>1st setup</h1>
+        </section>
+        <section className="w-4/12 min-h-screen bg-blue-500 bg-opacity-50">
+
+        </section>
+    </main>  
+    
+    )
+}
+export default ecalendarPage
